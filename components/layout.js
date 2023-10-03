58e63a8bcaf55aa31d50ae9c8700b17b7c0a6585
user <user@MacBook-Pro-user.local>
2023-10-03 07:38:44 +0300

    Add i18n html lang attribute

--- components/layout.js
+++ components/layout.js
@@ -22,6 +22,7 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+
       <header className={styles.header}>
         {home ? (
           <>
@@ -55,7 +56,9 @@ export default function Layout({ children, home }) {
           </>
         )}
       </header>
+
       <main>{children}</main>
+
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">← Back to home</Link>
